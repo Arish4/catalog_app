18989f70e4aaecf4b64a1a07a268a7bb3c32d64a
Arish <mohdarish062@gmail.com>
2023-09-08 15:37:20 +0530

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'store.dart';
+import 'cart_page.dart';
+import 'login_page.dart';
+import 'routes.dart';
+import 'package:velocity_x/velocity_x.dart';
+import 'home_page.dart';
+import 'themes.dart';
+
+void main() {
+  runApp(VxState(store: MyStore(), child: MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      themeMode: ThemeMode.system,
+      theme: MyTheme.lightTheme(context),
+      darkTheme: MyTheme.darkTheme(context),
+      debugShowCheckedModeBanner: false,
+      initialRoute: MyRoutes.homeRoute,
+      routes: {
+        "/": (context) => LoginPage(),
+        MyRoutes.homeRoute: (context) => HomePage(),
+        MyRoutes.loginRoute: (context) => LoginPage(),
+        MyRoutes.cartRoute: (context) => CartPage(),
+      },
+    );
+  }
+}
---
+++ lib/store.dart
@@ -0,0 +1,14 @@
+import 'cart.dart';
+import 'catalog.dart';
+import 'package:velocity_x/velocity_x.dart';
+
+class MyStore extends VxStore {
+  CatalogModel catalog;
+  CartModel cart;
+
+  MyStore() {
+    catalog = CatalogModel();
+    cart = CartModel();
+    cart.catalog = catalog;
+  }
+}
